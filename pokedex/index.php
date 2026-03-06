90c72728257d5d97cb2595a1b194f8d755acbfdd
TimZer <tim.degoeij@edu-kw1c.nl>
2026-03-06 10:27:02 +0100

    begin in een pokedex database

--- pokedex/index.php
+++ pokedex/index.php
@@ -1,1 +1,25 @@
 <?php
+
+
+include "../includes/db_functions.php";
+
+StartConnection("pokemondb");
+
+//$query = "SELECT * FROM `pokemon` WHERE type1 = 'Water' OR type1 = 'Fire'";
+
+$query = "SELECT * FROM `pokemon`";
+
+$result = ExecuteSelectQuery($query);
+
+foreach ($result as $row)
+{
+    $name = $row["name"];
+    $img = $row["picture"];
+    echo "<article>";
+
+        echo $name."<br>";
+        echo "<img src='$img' alt='$name' width='75'>";
+
+    echo "</article>";
+}
+
