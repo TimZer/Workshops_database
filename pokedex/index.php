d74c644cfc781edc0b6f60591d559cb4ccba5908
TimZer <tim.degoeij@edu-kw1c.nl>
2026-03-16 12:40:53 +0100

    verandering aan de index van de pokedex en de style van de pokedex

--- pokedex/index.php
+++ pokedex/index.php
@@ -6,69 +6,76 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href=style.css>
 </head>
 <body>
-
-    <h1>Pokedex</h1>
-
+<header>
+    <a href="index.php">
+        <h1>Heel veel sigma pokemons</h1>
+    </a>
     <form action="index.php" method="get">
-
         <fieldset>
+            <legend>zoeken</legend>
+            <label>Zoeken</label>
+            <input type="text" name="searchName">
+            <input type="submit" name="searchForm" value="zoeken">
 
-        <legend>Zoeken</legend>
-        <input type="text" name="searchName">
-        <input type="submit" name="searchName" value="Zoeken">
+            <select name="searchType1">
+                <?php
+
+                include "../includes/db_functions.php";
+
+                StartConnection("pokemondb");
+
+
+                    $type1Query = "SELECT DISTINCT type1 FROM pokemon;";
+
+                    $type1 = ExecuteSelectQuery($type1Query);
+
+                    foreach ($type1 as $item) {
+                        echo "<option ='" . $item['type1'] . "'>" . $item['type1'] . "</option>";
+                }
+                ?>
+            </select>
 
         </fieldset>
+
+    </form>
+</header>
 <main>
+<?php
 
-    <?php
+//code om het zoekveld te laten functioneren
+if(isset($_GET["searchForm"]))
+{
+    $searchName = $_GET["searchName"];
+    $searchType1 = $_GET["searchType1"];
 
-
-
-        include "../includes/db_functions.php";
-
-        StartConnection("pokemondb");
-
-       //code om zoekveld te laten werken
-        if (isset($_GET["searchName"]))
-        {
-              $searchName = $_GET["searchName"];
-
-              $query = "SELECT * FROM pokemon WHERE name LIKE '%$searchName%';";
-        }
-        else
-        {
-            $query = "SELECT * FROM pokemon;";
-        }
-
+    if(isset($searchType1))
+    {
+        $query = "SELECT * FROM pokemon WHERE type1 ='$searchType1' AND name LIKE '%$searchName%';";
+}
+    else {
+        $query = "SELECT * FROM pokemon WHERE name LIKE '%$searchName%';";
+    }
+}
+else{
+    $query = "SELECT * FROM pokemon;";
+}
 
 
-        //$query = "SELECT * FROM `pokemon` WHERE type1 = 'Water' OR type1 = 'Fire'";
+$result = ExecuteSelectQuery($query);
 
-    $result = ExecuteSelectQuery($query);
-
-        foreach ($result as $row)
-            {
-                $name = $row["name"];
-                $img = $row["picture"];
-                echo "<article>";
-
-                echo $name."<br>";
-                echo "<img src='$img' alt='$name' width='75'>";
-
-                echo "</article>";
-            }
-        ?>
+foreach($result as $row)
+{
+    $name = $row["name"];
+    $img = $row["picture"];
+    echo "<article>";
+    echo $row["name"]. "<br>";
+    echo "<img src='$img' alt='$name' width='50'>";
+    echo "</article>";
+}
+?>
 </main>
-
-
 </body>
 </html>
-
-
-
-
-
-
